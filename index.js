ec783e51a79981894e1a45ffdddeb3ae320ca75b
superSimpleJahir <supersimple.jahir@gmail.com>
2023-08-02 21:36:25 +0600

    secend commit

--- index.js
+++ index.js
@@ -115,3 +115,15 @@ var counter = 0;
 counter += 5;
 counter += 3;
 console.log(counter);
+
+console.log(2 + 2);
+// Expected output: 4
+
+console.log(2 + true);
+// Expected output: 3
+
+console.log('hello ' + 'everyone');
+// Expected output: "hello everyone"
+
+console.log(2001 + ': A Space Odyssey');
+// Expected output: "2001: A Space Odyssey"
